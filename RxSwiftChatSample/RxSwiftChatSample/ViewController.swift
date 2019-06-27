786a2ab657e77e88b3cb6d6c303ea5b8ff9a09b5
Kazuya Hirobe <k.hirobe@gmail.com>
2019-06-27 20:21:11 +0900

    Add initial project

---
+++ RxSwiftChatSample/RxSwiftChatSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftChatSample
+//
+//  Created by hirobe on 2019/06/27.
+//  Copyright © 2019 Bunguu inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
